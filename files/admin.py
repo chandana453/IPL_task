5ebe97a3c523211f7accf824724debb58fb230c8
Your Name <you@example.com>
2020-11-24 21:14:53 +0530

    first commit

---
+++ files/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+# Register your models here.
+
+from django.contrib import admin
+
+# Register your models here.
+# Register your models here.
+from .models import profile_data
+admin.site.register(profile_data)
